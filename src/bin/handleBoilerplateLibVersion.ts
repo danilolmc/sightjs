c30740d20f7828ab178797468dd5525bf839d293
DaniloLima122 <limadanilo06@gmail.com>
2024-12-30 11:26:50 -0300

    fix: it fixes bin issue

--- src/bin/handleBoilerplateLibVersion.ts
+++ src/bin/handleBoilerplateLibVersion.ts
@@ -4,22 +4,20 @@ import { dirname, resolve } from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 
-export function handleBoilerplateLibVersion() {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-  const packageJsonPath = resolve(__dirname, './quickstart_app/package.json');
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+const packageJsonPath = resolve(__dirname, './quickstart_app/package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-  const version = process.env.VERSION;
+const version = process.env.VERSION;
 
-  if (packageJson.dependencies) {
-    packageJson.dependencies.vistajs = `^${version}`;
-  } else {
-    packageJson.dependencies = {
-      vistajs: `^${version}`,
-    };
-  }
-
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+if (packageJson.dependencies) {
+  packageJson.dependencies.vistajs = `^${version}`;
+} else {
+  packageJson.dependencies = {
+    vistajs: `^${version}`,
+  };
 }
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
